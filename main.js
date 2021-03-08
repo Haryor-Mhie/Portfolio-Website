05601ecbf86f21a3290af0a47db2723a232a96a9
Haryor-Mhie <69142285+Haryor-Mhie@users.noreply.github.com>
2021-03-08 17:05:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$("#radio-btn1").click(function(){
+  $(".selected1").toggle('slow');
+});
+    
+$("#radio-btn2").click(function(){
+  $(".selected2").toggle('slow');
+});
+    
+$("#radio-btn3").click(function(){
+  $(".selected3").toggle('slow');
+});
+    
